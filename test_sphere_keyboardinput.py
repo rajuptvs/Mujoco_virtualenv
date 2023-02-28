906075219b81e5576a310f259ecc25dd53571f99
rajuptvs <raju.ptvs@gmail.com>
2023-02-28 13:48:13 -0800

    keyboard input

--- test_sphere_keyboardinput.py
+++ test_sphere_keyboardinput.py
@@ -5,25 +5,23 @@ import glfw
 # Define the key callback function
 def key_callback(window, key, scancode, action, mods):
     if key == glfw.KEY_RIGHT and action == glfw.PRESS:
-        # Apply a force to the ball when the spacebar is pressed
         ball_id = sim.model.body_name2id('ball')
-        sim.data.xfrc_applied[ball_id,0] = 0.8 # Add a force of 10 in the x direction
+        sim.data.xfrc_applied[ball_id,0] = 1.0
         print("right key pressed and registered")
     if key == glfw.KEY_LEFT and action == glfw.PRESS:
-        # Apply a force to the ball when the spacebar is pressed
         ball_id = sim.model.body_name2id('ball')
-        sim.data.xfrc_applied[ball_id,0] = -0.8 # Add a force of 10 in the x direction
+        sim.data.xfrc_applied[ball_id,0] = -1.0
         print("left key pressed and registered")
     if key == glfw.KEY_UP and action == glfw.PRESS:
         # Apply a force to the ball when the spacebar is pressed
         ball_id = sim.model.body_name2id('ball')
-        sim.data.xfrc_applied[ball_id,1] = 0.8 
+        sim.data.xfrc_applied[ball_id,1] = 1.0
         # sim.data.xfrc_applied[ball_id,0] = -0.8 
         print("up key pressed and registered")
     if key == glfw.KEY_DOWN and action == glfw.PRESS:
         # Apply a force to the ball when the spacebar is pressed
         ball_id = sim.model.body_name2id('ball')
-        sim.data.xfrc_applied[ball_id,1] = -0.8 
+        sim.data.xfrc_applied[ball_id,1] = -1.0 
         print("down key pressed and registered")
         
 # Load the Mujoco model and create a simulation
